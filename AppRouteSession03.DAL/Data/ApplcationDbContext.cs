e15356e41100cae28bfa536d38421c8b489cec8f
Abdulrahman Awad <abdulrahmanawad7861991@Gmail.com>
2024-04-03 00:16:49 +0200

    Microsoft Identity Package

--- AppRouteSession03.DAL/Data/ApplcationDbContext.cs
+++ AppRouteSession03.DAL/Data/ApplcationDbContext.cs
@@ -8,10 +8,12 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using App.DAL.Models;
 using AppRouteSession03.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace AppRouteSession03.PL
 {
-    public class ApplecationDbContext : DbContext
+    public class ApplecationDbContext : IdentityDbContext
     {
 
         public ApplecationDbContext(DbContextOptions<ApplecationDbContext> options) : base(options)
@@ -20,11 +22,14 @@ namespace AppRouteSession03.PL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<IdentityRole>().ToTable("Rols");
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+
     }
 
 }
